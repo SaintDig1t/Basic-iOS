6c35f59ab0ae339b6c999b9ebd7d70aa66292b8b
Henry <128566178+SaintDig1t@users.noreply.github.com>
2023-06-28 08:24:11 -0400

    Add files via upload

---
+++ ParentView.swift
@@ -0,0 +1,20 @@
+//
+//  ParentView.swift
+//  
+//
+//  Created by Henry Rivera Osso on 6/27/23.
+//
+
+import SwiftUI
+
+struct ParentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ParentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ParentView()
+    }
+}
